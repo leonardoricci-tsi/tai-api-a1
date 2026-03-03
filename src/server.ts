fe6538146c79b37e952ed258d8b1ee64e17a3555
leonardoricci-tsi <lo5642050@gmail.com>
2026-03-03 01:30:19 +0000

    feat: seerver structure

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (Req : Request, res : Response) => {
+    res.send("Olá Express!!!");
+});
+
+app.get("/saudacao/:nome",(req: Request, res: Response)=>{
+    const nome = req.params.nome;
+    res.send("Olá "+ nome)
+
+})
+
+app.listen(PORT,() => {
+    console.log("Servidor executando em localhost")
+});
